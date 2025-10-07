537af852373a0550d88b4ccc4b3fdadba0fb8044
MatthiasVanDeurenPXL <116798860+NotMatthias@users.noreply.github.com>
2025-10-07 16:39:50 +0200

    Fix subnet_id reference in aws_instance resource

--- main.tf
+++ main.tf
@@ -150,7 +150,7 @@ resource "aws_security_group" "web_sg" {
     resource "aws_instance" "first_instance" {
   ami           = "ami-0360c520857e3138f"
   instance_type = "t3.micro"
-  subnet_id = aws_subnet.public_subnet[0]
+  subnet_id = aws_subnet.public_subnet[0].id
   associate_public_ip_address = true
   user_data = base64encode(
     <<-EOF
